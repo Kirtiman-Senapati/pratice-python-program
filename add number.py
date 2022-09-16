105d5eb591ed9b232a12f175fddf30d65f35aaa9
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2022-09-16 23:27:30 +0530

    Addition of 2  numbers in Python

--- add number.py
+++ add number.py
@@ -1,9 +1,10 @@
 # Python3 program to add two numbers
-num1 = 15 
-num2 = 12
+num1 = 10
+num2 = 50
 
 # Adding two nos
 sum = num1 + num2
 
 # printing values
 print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
+# it works perfectly
